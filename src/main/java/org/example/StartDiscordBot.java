99a7521e6467fcc584c04738bdd883d1f9ba12e2
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-02-23 19:51:14 +0900

    コメント追加

--- src/main/java/org/example/StartDiscordBot.java
+++ src/main/java/org/example/StartDiscordBot.java
@@ -3,6 +3,9 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Start bot.
+ */
 @SpringBootApplication
 public class StartDiscordBot {
   public static void main(String[] args) {
